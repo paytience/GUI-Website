13c1f3f1a22be6b3e5d28b53dd719637e95020c9
paytience <47988962+paytience@users.noreply.github.com>
2020-03-28 15:08:02 +0100

    Update Startup.cs

--- CyborgWeb/Startup.cs
+++ CyborgWeb/Startup.cs
@@ -31,6 +31,8 @@ namespace CyborgWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = new 
+            List<string> { "index.cshtml" } });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
